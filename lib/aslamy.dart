ec91e5b9f384c601047ecea402114b75efe5e0fc
sivoadel <selvinadel816@gmail.com>
2021-10-29 22:20:01 +0200

    add homepage

---
+++ lib/aslamy.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+class Homepage extends StatelessWidget{
+  static const String routeName='home';
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body:Container(
+
+      ),
+    );
+  }
+}
