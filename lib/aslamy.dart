fa847d70fedd57a62e0620b2b7030617bce0c597
sivoadel <selvinadel816@gmail.com>
2021-10-30 23:06:49 +0200

    add homepage

--- lib/aslamy.dart
+++ lib/aslamy.dart
@@ -3,10 +3,36 @@ class Homepage extends StatelessWidget{
   static const String routeName='home';
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body:Container(
+    return Stack(
+      children: [
+        Image.asset('assets/images/screen.png',
 
-      ),
+        ),
+
+        Scaffold(
+          backgroundColor: Colors.transparent,
+          bottomNavigationBar: BottomNavigationBar(
+            items: [
+              BottomNavigationBarItem(
+                label: '',
+                  icon:Image.asset('assets/images/ms7f.png') ),
+              BottomNavigationBarItem(
+                  label: '',
+                  icon:Image.asset('assets/images/iconsb7a.png') ),
+              BottomNavigationBarItem(
+                  label: '',
+                  icon:Image.asset('assets/images/book.png') ),
+              BottomNavigationBarItem(
+                  label: '',
+                  icon:Image.asset('assets/images/radio.png') )
+            ],
+          ),
+          body:Container(
+
+
+          ),
+        ),
+      ],
     );
   }
 }
